0ae0dbee6ae4fbd92faf86467babf6768d7ea137
Benny Rinaldo <rinaldobdg@yahoo.com>
2022-02-18 10:26:46 +0700

    ADD: Section for the TESLA

---
+++ src/components/Home.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+
+function Home() {
+  return (
+    <Container>
+        <Section 
+            title="Model S"
+            desc="Order Online for Touchless Delivery"
+            backgroundImg="model-s.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"        
+        />
+        <Section 
+            title="Model 3"
+            desc="Order Online for Touchless Delivery"
+            backgroundImg="model-3.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"  
+        />
+        <Section 
+            title="Model X"
+            desc="Order Online for Touchless Delivery"
+            backgroundImg="model-x.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"  
+        />
+        <Section 
+            title="Model Y"
+            desc="Order Online for Touchless Delivery"
+            backgroundImg="model-y.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"  
+        />
+        <Section 
+            title="Lower Cost Solar Panels in America"
+            desc="Money-back Guarantee"
+            backgroundImg="solar-panel.jpg"
+            leftBtnText="Order now"
+            rightBtnText="Learn more"  
+        />
+
+        <Section 
+            title="Solar for New Roofs"
+            desc="Solar Roof Costs Less Than A New Roof Plus Solar Panels"
+            backgroundImg="solar-roof.jpg"
+            leftBtnText="Order now"
+            rightBtnText="Learn more"  
+        />
+
+        <Section 
+            title="Accessories"
+            desc=""
+            backgroundImg="accessories.jpg"
+            leftBtnText="Shop now"
+            rightBtnText=""  
+        />
+
+    </Container>
+    
+  )
+}
+
+export default Home
+
+const Container = styled.div`
+    height: 100vh;
+    
+
+`
---
+++ src/components/Section.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Section( { title, desc, leftBtnText, rightBtnText, backgroundImg } ) {
+    
+  return (
+    <Wrap bgImage = {backgroundImg}>
+        <ItemText>
+            <h1>{ title }</h1>
+            <p>{ desc } </p>
+        </ItemText>
+        <Button>
+            <ButtonGroup>
+                <LeftButton>
+                    { leftBtnText }
+                </LeftButton>
+
+                { rightBtnText && 
+                <RightButton>
+                    { rightBtnText }
+                </RightButton>
+                }       
+            </ButtonGroup>
+            <DownArrow src="/images/down-arrow.svg" />
+        </Button>
+        
+    </Wrap>
+  )
+}
+
+export default Section
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-size: cover; 
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('images/model-s.jpg');
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; //for vertical
+    align-items: center; //horizontal
+    background-image: ${props => `url("/images/${props.bgImage}")`}
+`
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-botton: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
+
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin: 8px;
+`
+
+const RightButton = styled(LeftButton)`
+    background: white;
+    opacity: 0.65;
+    color: black;
+`
+
+const DownArrow = styled.img`
+    height: 40px;
+    overflow-x: hidden;
+    animation: animateDown infinite 1.5s;
+`
+
+const Button = styled.div`
+`
